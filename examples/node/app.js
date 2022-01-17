8ac762edb247441661162c98f94dfc9b2bf660e6
Alexander Wunschik <alexander.wunschik@fullstax.de>
2022-01-17 11:07:43 +0100

    docs: add node and browser examples

---
+++ examples/node/app.js
@@ -0,0 +1,7 @@
+import { compute } from '../../lib/index.js';
+
+const timeToEvents = [1, 12, 22, 29, 31, 36, 38, 50, 60, 61, 70, 88, 99, 110, 140];
+const events = [ false, true, false, true, true, false, false, false, false, true, true, false, false, false, false];
+
+const kmData = compute(timeToEvents, events);
+console.table(kmData);
